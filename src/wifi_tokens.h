1fc42a020fbb04b44494dee645fca06e459edd90
douglas freymann <dmfreymann@gmail.com>
2020-04-15 18:04:40 -0500

    changed naming + don't want this here

--- src/wifi_tokens.h
+++
@@ -1,9 +0,0 @@
-// 3.24.20 for future use.  
-//
-// this is just initial setup code from chirp-i2c-revised that 
-// I've removed from the git-managed source 
-
-    // One time use:
-    // WiFi.setCredentials("BijvoetJaldiExt","9412441646");
-
-
