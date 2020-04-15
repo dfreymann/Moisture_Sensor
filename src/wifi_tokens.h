c42546156c8db27abcc2c47e810b32096a450a30
dfreymann <info@jaldilabs.org>
2020-04-15 18:03:17 -0500

    gitignore and tokens reference change

--- src/wifi_tokens.h
+++ src/wifi_tokens.h
@@ -1,7 +1,9 @@
 // 3.24.20 for future use.  
 //
-// this is just initial setup code from chirp-i2c-revised that I've removed from the 
-// git-managed source 
+// this is just initial setup code from chirp-i2c-revised that 
+// I've removed from the git-managed source 
 
     // One time use:
     // WiFi.setCredentials("BijvoetJaldiExt","9412441646");
+
+
